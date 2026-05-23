4e46067634277a6dd18553da055353fb27ba0d1d
lch <lch24tsinghua@qq.com>
2026-05-23 13:34:43 +0800

    api_key

--- CAPI/cpp/API/include/API.h
+++ CAPI/cpp/API/include/API.h
@@ -93,6 +93,7 @@ public:
     // 等待下一帧
     virtual bool Wait() = 0;
     virtual std::future<bool> EndAllAction() = 0;
+    virtual std::future<std::string> AskAI(int64_t currentGameTime, std::string prompt, std::string apiKey = "") = 0;
     [[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI9::Character>> GetCharacters() const = 0;
     [[nodiscard]] virtual std::vector<std::shared_ptr<const THUAI9::Character>> GetEnemyCharacters() const = 0;
     [[nodiscard]] virtual std::vector<std::vector<THUAI9::PlaceType>> GetFullMap() const = 0;
@@ -190,6 +191,7 @@ public:
     [[nodiscard]] int32_t GetFrameCount() const override;
     bool Wait() override;
     std::future<bool> EndAllAction() override;
+    std::future<std::string> AskAI(int64_t currentGameTime, std::string prompt, std::string apiKey = "") override;
 
     std::future<bool> Move(int64_t moveTimeInMilliseconds, double angle) override;
     std::future<bool> MoveRight(int64_t timeInMilliseconds) override;
@@ -258,6 +260,7 @@ public:
     [[nodiscard]] int32_t GetFrameCount() const override;
     bool Wait() override;
     std::future<bool> EndAllAction() override;
+    std::future<std::string> AskAI(int64_t currentGameTime, std::string prompt, std::string apiKey = "") override;
 
     [[nodiscard]] std::vector<std::shared_ptr<const THUAI9::Character>> GetCharacters() const override;
     [[nodiscard]] std::vector<std::shared_ptr<const THUAI9::Character>> GetEnemyCharacters() const override;
@@ -307,6 +310,7 @@ public:
     bool Wait() override;
     [[nodiscard]] int32_t GetFrameCount() const override;
     std::future<bool> EndAllAction() override;
+    std::future<std::string> AskAI(int64_t currentGameTime, std::string prompt, std::string apiKey = "") override;
 
     std::future<bool> Move(int64_t moveTimeInMilliseconds, double angle) override;
     std::future<bool> MoveRight(int64_t timeInMilliseconds) override;
@@ -366,6 +370,7 @@ public:
     [[nodiscard]] int32_t GetFrameCount() const override;
     bool Wait() override;
     std::future<bool> EndAllAction() override;
+    std::future<std::string> AskAI(int64_t currentGameTime, std::string prompt, std::string apiKey = "") override;
 
     [[nodiscard]] std::vector<std::shared_ptr<const THUAI9::Character>> GetCharacters() const override;
     [[nodiscard]] std::vector<std::shared_ptr<const THUAI9::Character>> GetEnemyCharacters() const override;
